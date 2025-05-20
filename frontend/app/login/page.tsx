dbd21332aadc9b043fd645eb11ee6cc5b32f6790
Prabesh2025 <prabeshneupane231@gmail.com>
2025-05-20 11:06:40 +0545

    next js

---
+++ frontend/app/login/page.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import React from "react";
+
+export default function page() {
+  return (
+    <form>
+      
+
+      <div className="w-4/12 mx-auto mt-10 shadow-lg border rounded-md py-10 px-4">
+        <h1 className="text-2xl font-semibold text-center">Login</h1>
+
+        <div className="flex flex-col gap-4">
+          <label>Email:</label>
+          <input
+            type="email"
+            name="email"
+            className="border rounded-md px-2 py-1 w-80"
+            required
+          />
+
+          <label>Password:</label>
+          <input
+            type="password"
+            name="password"
+            className="border rounded-md px-2 py-1 w-80"
+            required
+          />
+
+          <button
+            type="submit"
+            className="bg-blue-500 w-50 text-white rounded-md px-4 py-2 hover:bg-blue-600 "
+          >
+            Login
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+}
